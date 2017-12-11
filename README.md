014bc3d91edc9192505f08dc5d4fba1e066c0c3e
dav2113534 <dpalacios03081991@gmail.com>
2017-12-10 20:33:26 -0700

    added styling to wrapper

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Mrshowbot
 -Create random quote generator
 -add css for homepage
+
+
+
+
+
+
+
+        Resoures
+Quotes: https://www.magicalquote.com/series/mr-robot/page/10/
--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-function randomGen() {
-    return console.log(quotes);
-}
+// function randomLoop() {
+//     for (var i = 0; i < quotes.length; i++) {
+//         return Math.floor(Math.random() * i)
+//     }
+// }
 
+$('#random').hover(function () {
+    $('h2').text(randomQuote(quotes.length));
+})
+
+function randomQuote(list) {
+    var i = Math.floor(Math.random() * list);
+    return list[i];
+}
