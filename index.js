22d84b582a1967cbb63e040f20443bdff38223cf
dav2113534 <dpalacios03081991@gmail.com>
2017-12-10 18:23:15 -0700

    adding quotes

---
+++ index.js
@@ -0,0 +1,34 @@
+let quotes = [{
+        name: "Phillip Price",
+        quote: "World catastrophes like this, they aren't caused by lone wolves like you." +
+            "They occur because men like me allow them. You just happened to stumble into one."
+    },
+    {
+        name: "Phillip Price",
+        quote: "You can't force an agenda, Mr. Alderson. You have to inspire one."
+    },
+    {
+        name: "",
+        quote: ""
+    },
+    {
+        name: "",
+        quote: ""
+    },
+    {
+        name: "",
+        quote: ""
+    },
+    {
+        name: "",
+        quote: ""
+    },
+    {
+        name: "",
+        quote: ""
+    },
+    {
+        name: "",
+        quote: ""
+    }
+]
